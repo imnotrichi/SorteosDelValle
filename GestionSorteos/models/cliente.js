7bcdf87d25fcdafd98073bf696f15f56f347ba6f
abel2606 <150111881+abel2606@users.noreply.github.com>
2025-11-08 23:11:20 -0700

    Duplicación de usuarios, clientes y organizadores

---
+++ GestionSorteos/models/cliente.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Cliente extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Cliente.belongsTo(models.Usuario, { foreignKey: "id_usuario" });
+      Cliente.hasMany(models.Numero, {foreignKey: 'id_cliente'})
+    }
+  }
+  Cliente.init({
+    id_usuario: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false
+    }
+
+  }, {
+    sequelize,
+    modelName: 'Cliente',
+    tableName: 'Clientes'
+  });
+  return Cliente;
+};
---
+++ GestionSorteos/models/usuario.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Usuario extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Usuario.hasOne(models.Cliente, {foreignKey: "id_usuario"});
+      Usuario.hasOne(models.Organizador, {foreignKey: "id_usuario"});
+    }
+  }
+  Usuario.init({
+    nombres: DataTypes.STRING,
+    apellido_paterno: DataTypes.STRING,
+    apellido_materno: DataTypes.STRING,
+
+  }, {
+    sequelize,
+    modelName: 'Usuario',
+    tableName: 'Usuarios'
+  });
+  return Usuario;
+};
