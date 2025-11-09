c4fb72046424d99ca2dcd1406848adb633d9fb49
abel2606 <150111881+abel2606@users.noreply.github.com>
2025-11-09 00:04:04 -0700

    Update organizadorsorteo.js

--- GestionSorteos/models/organizadorsorteo.js
+++ GestionSorteos/models/organizadorsorteo.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      OrganizadorSorteo.belongsTo(models.Organizar, {
+      OrganizadorSorteo.belongsTo(models.Organizador, {
         foreignKey: 'id_organizador',
         targetKey: 'id_usuario'
       });
