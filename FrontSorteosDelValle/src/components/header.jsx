7d7179d6408e08b41b4625b3622bfa3f593ca19d
abel2606 <150111881+abel2606@users.noreply.github.com>
2025-11-10 22:32:03 -0700

    Maquetado de mis sorteos

--- FrontSorteosDelValle/src/components/header.jsx
+++ FrontSorteosDelValle/src/components/header.jsx
@@ -2,20 +2,29 @@ import React from 'react';
 import logo from '../assets/logo.png';
 import userIcon from '../assets/usuario.png';
 
-const Header = () => {
+const Header = ({onNavigate}) => {
   return (
     <header className="sticky top-0 z-20 w-full bg-background-light/80 backdrop-blur-sm border-b border-border-light">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
           <div className="flex items-center gap-4 text-text-light dark:text-text-dark">
-            <img src={logo} alt="Sorteos del Valle" className="h-35 w-auto" />
+            <button onClick={() => onNavigate('misSorteos')} className="cursor-pointer">
+              <img src={logo} alt="Sorteos del Valle" className="h-35 w-auto" />
+            </button>
           </div>
           <div className="flex flex-1 justify-end gap-2 items-center">
             <nav className="hidden md:flex items-center gap-9 mr-7">
-              <a href="#" className="text-sm font-medium hover:text-primary">Mis Sorteos</a>
-              <a href="#" className="text-sm font-medium hover:text-primary">Reportes</a>
+                <button onClick={() => onNavigate('misSorteos')} className="text-sm font-medium hover:text-primary">
+                  Mis Sorteos
+                </button>
+
+                <button 
+                //TODO: onClick={() => onNavigate('reportes')} // <-- Futuro
+                className="text-sm font-medium hover:text-primary">
+                Reportes
+                </button>
             </nav>
-            <button className="hidden sm:flex items-center justify-center rounded-lg h-10 px-4 bg-primary hover:bg-primary/90 text-text-light text-sm font-bold">
+            <button onClick={() => onNavigate('crearSorteo')} className="hidden sm:flex items-center justify-center rounded-lg h-10 px-4 bg-primary hover:bg-primary/90 text-text-light text-sm font-bold">
               Crear sorteo
             </button>
             <div className="flex items-center gap-2 pl-4">
---
+++ FrontSorteosDelValle/src/gestionSorteo/misSorteos.jsx
@@ -0,0 +1,32 @@
+import { SorteoCard } from "../components/sorteoCard";
+const MisSorteos = ({onNavigate}) => {
+
+    const mockSorteos = [
+    { id: 1, titulo: "Rifa de Xbox",imagen: 'https://m.media-amazon.com/images/I/71AtLTRlT2L._AC_UF1000,1000_QL80_.jpg', estado: "Activo" },
+    { id: 2, titulo: "Viaje de ensueño. Playa paradisiaca te llama", imagen: 'https://media.gq.com.mx/photos/620e915c43f71a078a35533f/master/pass/playa.jpg',estado: "Activo"},
+    { id: 3, titulo: "Cocina nueva cocina para tu hogar",imagen: 'https://www.viacelere.com/wp-content/uploads/old-blog/2017/10/tipos-de-cocina_opt.jpg', estado: "Finalizado"},
+    { id: 4, titulo: "Rifa kit gamer",imagen: 'https://www.cyberpuerta.mx/img/product/M/CP-NACEBTECHNOLOGY-NA-0934-7e92f0.jpg', estado: "Finalizado"},
+  ];
+  return (
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 font-body">
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-[32px] font-extrabold tracking-tight text-text-light">
+          Mis Sorteos
+        </h1>
+
+      </div>
+
+      <div className="grid grid-cols-3 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        {mockSorteos.map((sorteo) => (
+          <SorteoCard 
+            key={sorteo.id} 
+            sorteo={sorteo}
+            onEditarClick={() => onNavigate('editarSorteo', sorteo.id)} 
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MisSorteos;
