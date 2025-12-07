19d1ee9f42cde2656aa18a9d4d59f6221aa5226d
imnotrichi <ricardo.gutierrez247398@potros.itson.edu.mx>
2025-12-07 02:22:16 -0700

    marcar número como pagado ya funciona

--- GestionSorteos/index.js
+++ GestionSorteos/index.js
@@ -6,6 +6,7 @@ dotenv.config();
 const sorteosRouter = require('./routes/sorteosRouter.js');
 const usuariosRouter = require('./routes/usuariosRouter.js');
 const numerosRouter = require('./routes/numerosRouter.js');
+const pagosRouter = require('./routes/pagosRouter.js');
 
 const { AppError, globalErrorHandler } = require('./utils/appError.js');
 
@@ -17,6 +18,7 @@ app.use(morgan('combined'));
 app.use('/api/sorteos', sorteosRouter);
 app.use('/api/sorteos/usuarios', usuariosRouter);
 app.use('/api/numeros', numerosRouter);
+app.use('/api/pagos', pagosRouter);
 
 app.use((req, res, next) => {
     const error = new AppError(`No se ha podido acceder a ${req.originalUrl} en el servidor`, 404);
--- GestionSorteos/routes/numerosRouter.js
+++ GestionSorteos/routes/numerosRouter.js
@@ -4,7 +4,7 @@ const numerosController = require('../controllers/numerosController.js');
 const router = express.Router();
 
 router.post('/apartar', numerosController.apartarNumeros);
-router.post('/registrar-comprobante', numerosController.registrarComprobantePago);
+router.post('/marcar-pagados', numerosController.marcarNumerosComoPagados);
 router.get('/disponibles', numerosController.obtenerNumerosDisponibles);
 router.get('/apartados', numerosController.obtenerNumerosApartados);
 router.delete('/liberar', numerosController.liberarNumeros);
