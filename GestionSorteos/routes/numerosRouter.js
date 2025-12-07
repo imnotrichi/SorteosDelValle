e807d05f741e135974039db0a2a236bc23d6ce8b
imnotrichi <ricardo.gutierrez247398@potros.itson.edu.mx>
2025-12-07 14:02:38 -0700

    se obtienen los números de un cliente

--- GestionSorteos/routes/numerosRouter.js
+++ GestionSorteos/routes/numerosRouter.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.post('/apartar', numerosController.apartarNumeros);
 router.post('/marcar-pagados', numerosController.marcarNumerosComoPagados);
 router.get('/disponibles', numerosController.obtenerNumerosDisponibles);
+router.get('/cliente/:id', numerosController.obtenerNumerosCliente);
 router.get('/apartados', numerosController.obtenerNumerosApartados);
 router.delete('/liberar', numerosController.liberarNumeros);
 
